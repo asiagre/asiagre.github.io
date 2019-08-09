a04526d10321a966a63e9e1ba99b18be01526819
asiagre <33787360+asiagre@users.noreply.github.com>
2019-08-09 21:25:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://morning-depths-31083.herokuapp.com/';
+  const apiRoot = 'https://morning-depths-31083.herokuapp.com/v1/';
   const trelloApiRoot = 'https://morning-depths-31083.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
